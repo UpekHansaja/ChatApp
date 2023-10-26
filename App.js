ad4d91ade3e24e6f18243a19d90faf90a918199d
Upek Hansaja <upekhansajabk@gmail.com>
2023-10-27 00:16:23 +0530

    watchmanconfig

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
+import { SignIn } from "./SignIn";
 import { Home } from "./Home";
 import { Chat } from "./Chat";
 
@@ -15,14 +15,14 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ title: "Create New Account" }}
-        />
-        <Stack.Screen
           name="SignIn"
           component={SignIn}
           options={{ title: "Chat App" }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ title: "Create New Account" }}
         />
         <Stack.Screen
           name="Home"
